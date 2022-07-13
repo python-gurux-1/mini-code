3e597110914c0576b4120a7e3e9c378cdbf3083d
python-gurux-1 <108383232+python-gurux-1@users.noreply.github.com>
2022-07-13 09:24:56 +0500

    Create osmodule.py

---
+++ osmodule.py
@@ -0,0 +1,15 @@
+# import os
+
+#path = file ishlayotgan joy
+#path ni olish
+# path = os.getcwd()
+# print("1: ",path)
+
+#path ni almashtirish
+# os.chdir("/home/smartcoderuzb")# D://myfolder/
+# path = os.getcwd()
+# print("2: ",path)
+
+#papkadagi file va papkalar ro'yhatini olish
+# royhat = os.listdir()
+# print(royhat)
